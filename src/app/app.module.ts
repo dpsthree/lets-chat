57a73f809e83e33d805a62ed9fda71c4f0055063
Paul Spears <dpsthree@gmail.com>
2018-08-07 22:35:10 -0500

    Add AngularFire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -9,6 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA14rrxOtc0w1Km5MaW7MLe32MqhFLz3Y0',
+    authDomain: 'lets-chat-ng.firebaseapp.com',
+    databaseURL: 'https://lets-chat-ng.firebaseio.com',
+    projectId: 'lets-chat-ng',
+    storageBucket: 'lets-chat-ng.appspot.com',
+    messagingSenderId: '209732702637'
+  }
 };
